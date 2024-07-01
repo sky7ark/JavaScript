482d1790d0cbf73f56ed8055ab5bf68ca9cddd1a
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2024-07-01 04:35:12 -0230

    push pop u/shift

---
+++ push-pop-shift-unshift.js
@@ -0,0 +1,11 @@
+const array = [1, 2, 3, 4, 5];
+console.log(array);
+
+array.push(6); //add last
+console.log(array);
+array.pop(6); // remove last
+console.log(array);
+array.unshift(0); //add first
+console.log(array);
+array.shift(0); //remove first
+console.log(array);
