bcc8c10938d9eabfe7ebac522f2887da16044c76
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2024-07-01 04:05:34 -0230

    ternary operator

---
+++ if.js
@@ -0,0 +1,5 @@
+if (10 > 12) {
+  console.log("hello world");
+} else {
+  console.log("bye world");
+}
---
+++ multiple.js
@@ -0,0 +1,22 @@
+let price = 1100;
+const isLeader = false;
+
+// if (isLeader === true) {
+//   if (price > 1000) {
+//     price = price / 2;
+//     console.log(price);
+//   } else {
+//     console.log((price = 0));
+//   }
+// } else {
+//   price = price + 1000;
+//   console.log(price);
+// }
+//----------------------------------------//
+// ternary operator
+price =
+  isLeader === true
+    ? price > 1000
+      ? console.log(price / 2)
+      : console.log(0)
+    : console.log(price + 1000);
