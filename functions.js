e5cd487cfcf7b18549c66afe2d5c02be42a5f7b7
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2024-07-04 21:35:03 -0230

    functions added

---
+++ functions.js
@@ -0,0 +1,14 @@
+function evenNumbers(numbers) {
+  const evens = [];
+  for (const number of numbers) {
+    if (number % 2 === 0) {
+      console.log(number);
+      evens.push(number);
+    }
+  }
+  return evens;
+}
+
+const numbers = [1, 2, 4, 23, 34, 45, 67];
+const evens = evenNumbers(numbers);
+console.log(evens);
