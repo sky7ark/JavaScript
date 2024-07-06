0a686f719a727c2a69c1b1625688adae9979ae5c
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2024-07-06 03:22:54 -0230

    generating id with padstart, tostring and substring

--- padstart.js
+++ padstart.js
@@ -1,5 +1,14 @@
-const id = "1";
-const fullid = id.padStart(6, 0);
-const fullid2 = id.padEnd(6, 0);
-console.log(fullid);
-console.log(fullid2);
+const id = 1;
+const fullid = id.toString().padStart(6, 0);
+const fullid2 = id.toString().padEnd(6, 0);
+//console.log(fullid);
+//console.log(fullid2);
+
+const orderID = 1;
+console.log(orderID);
+const firstOrder = orderID.toString().padStart(4, 0);
+console.log(firstOrder);
+
+const secondOrder = parseInt(firstOrder.substring(firstOrder.length - 1)) + 1;
+const secondOrderID = secondOrder.toString().padStart(4, 0);
+console.log(secondOrderID);
